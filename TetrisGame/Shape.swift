c347c8934cbc163c44543515a3d0f41bf1f0cee7
AlexandraOlegovna <nikolenko.rnd@gmail.com>
2017-03-29 19:23:17 +0300

    add Tetriminos shapes

--- TetrisGame/Shape.swift
+++ TetrisGame/Shape.swift
@@ -41,3 +41,78 @@ enum Orientation: Int, CustomStringConvertible {
         return Orientation(rawValue:rotated)!
     }
 }
+
+let NumShapeTypes: UInt32 = 7
+
+let FirstBlockIdx: Int = 0
+let SecondBlockIdx: Int = 1
+let ThirdBlockIdx: Int = 2
+let FourthBlockIdx: Int = 3
+
+class Shape: Hashable, CustomStringConvertible {
+    // The color of the shape
+    let color:BlockColor
+    
+    // The blocks comprising the shape
+    var blocks = Array<Block>()
+    // The current orientation of the shape
+    var orientation: Orientation
+    // The column and row representing the shape's anchor point
+    var column, row:Int
+    
+    
+    var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [:]
+    }
+    
+
+    var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [:]
+    }
+    
+    var bottomBlocks:Array<Block> {
+        guard let bottomBlocks = bottomBlocksForOrientations[orientation] else {
+            return []
+        }
+        return bottomBlocks
+    }
+    
+    // Hashable
+    var hashValue:Int {
+        return blocks.reduce(0) { $0.hashValue ^ $1.hashValue }
+    }
+    
+    // CustomStringConvertible
+    var description:String {
+        return "\(color) block facing \(orientation): \(blocks[FirstBlockIdx]), \(blocks[SecondBlockIdx]), \(blocks[ThirdBlockIdx]), \(blocks[FourthBlockIdx])"
+    }
+    
+    init(column:Int, row:Int, color: BlockColor, orientation:Orientation) {
+        self.color = color
+        self.column = column
+        self.row = row
+        self.orientation = orientation
+        initializeBlocks()
+    }
+    
+    
+    convenience init(column:Int, row:Int) {
+        self.init(column:column, row:row, color:BlockColor.random(), orientation:Orientation.random())
+    }
+    
+    final func initializeBlocks() {
+        guard let blockRowColumnTranslations = blockRowColumnPositions[orientation] else {
+            return
+        }
+
+        
+        blocks = blockRowColumnTranslations.map { (diff) -> Block in
+            return Block(column: column + diff.columnDiff, row: row + diff.rowDiff, color: color)
+        }
+    }
+    
+}
+
+func ==(lhs: Shape, rhs: Shape) -> Bool {
+    return lhs.row == rhs.row && lhs.column == rhs.column
+}
